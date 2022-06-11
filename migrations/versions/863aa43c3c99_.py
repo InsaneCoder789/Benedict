d899acaf5b22a77c27cddce5ffaa85c641d5ae2d
RohanDeV <chatterjee@bot.com>
2022-06-11 10:56:00 +0400

    bug fixed
    Signed-off-by: RohanDeV <chatterjee@bot.com>

---
+++ migrations/versions/863aa43c3c99_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 863aa43c3c99
+Revises: 0f9cb69837f7
+Create Date: 2022-06-11 10:49:08.736863
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '863aa43c3c99'
+down_revision = '0f9cb69837f7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
